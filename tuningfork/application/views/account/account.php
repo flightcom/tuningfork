1666c3cd3a3d6e322dc349bb7e3eb0705bb020a7
sebastien <flightcom@wanadoo.fr>
2014-04-03 18:09:51 +0200

    Commit du 2014-04-03 18:10 @wb

---
+++ tuningfork/application/views/account/account.php
@@ -0,0 +1,53 @@
+<?php if($this->session->userdata('logged_in')) { ?>
+<ul class="nav navbar-nav navbar-right">
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Mon compte
+		<b class="caret"></b></a>
+	    <ul class="dropdown-menu">
+	        <li>
+	            <div class="navbar-content">
+	                <div class="row">
+	                    <div class="col-md-5">
+	                        <img src="http://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/twDq00QDud4/s120-c/photo.jpg"
+	                            alt="Alternate Text" class="img-responsive" />
+	                        <p class="text-center small">
+	                            <a href="#">Changer ma photo</a></p>
+	                    </div>
+	                    <div class="col-md-7">
+	                        <span><?php echo $this->session->userdata('user_prenom') . ' ' . $this->session->userdata('user_nom'); ?></span>
+	                        <p class="text-muted small">
+	                            <?php echo $this->session->userdata('user_email'); ?></p>
+	                        <div class="divider">
+	                        </div>
+	                        <a href="#" class="btn btn-primary btn-sm">Mon profil</a>
+	                        <?php if($this->session->userdata('user_isAdmin')) { ?>
+	                        	<?php if(strstr($this->router->class, 'admin') !== FALSE) { ?>
+	                        <a href="/" class="btn btn-primary btn-sm">Site</a>
+	                        <?php } else { ?>
+	                        <a href="/admin" class="btn btn-primary btn-sm">Admin</a>
+								<?php } ?>
+							<?php } ?>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="navbar-footer">
+	                <div class="navbar-footer-content">
+	                    <div class="row">
+	                        <div class="col-md-6">
+	                            <a href="#" class="btn btn-default btn-sm">Changer mon mot de passe</a>
+	                        </div>
+	                        <div class="col-md-6">
+	                            <a href="/connexion/deconnexion" class="btn btn-default btn-sm pull-right">Déconnexion</a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </li>
+	    </ul>
+	</li>
+</ul>
+<?php } else { ?>
+<ul class="nav navbar-nav navbar-right">
+	<li><a href="/connexion">Se connecter</a></li>
+</ul>
+<?php } ?>
