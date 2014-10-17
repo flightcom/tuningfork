4fc1ec21ba8ed1320e96242409e9dc27a97c7adc
Sébastien <flightcom@wanadoo.fr>
2014-10-17 08:05:50 +0200

    commit home 20141017@0800

--- tuningfork/application/views/admin/instruments/categories.php
+++ tuningfork/application/views/admin/instruments/categories.php
@@ -1,11 +1,22 @@
 <div ng-controller="AdminListCategCtrl">
 
-<ul ng-repeat="level in categories" class="col-xs-12 list-inline">
+<!-- <p>Categories Path : {{categoriesPath|json}}</p> -->
+<!-- <p>Categories : {{categories|json}}</p> -->
+
+<ul ng-repeat="level in categories" class="col-xs-12 list-inline list-categories">
+	<li><span>[ Niveau {{$index+1}} ]</span></li>
+	<li class="form-inline">
+		<input ng-show="showNewCategField" class="form-control form-inline" type="text" placeholder="Nouvelle catégorie..." ng-model="newcateg">
+		<button ng-hide="showNewCategField" ng-click="showNewCategField = !showNewCategField" class="btn btn-primary glyphicon glyphicon-plus"></button>
+		<button ng-show="showNewCategField" ng-click="addCateg($index);" class="btn btn-success glyphicon glyphicon-ok"></button>
+		<button ng-show="showNewCategField" ng-click="showNewCategField = !showNewCategField" class="btn btn-danger glyphicon glyphicon-remove"></button>
+	</li>
+	<li ng-hide="" ng-show></li>
 	<li ng-repeat="categorie in level">
 		<button class="btn" 
-			ng-class="{'btn-default': categoriesPath.indexOf(categorie.categ_id) == -1,
-				'btn-primary': categoriesPath.indexOf(categorie.categ_id) > -1}"
-			ng-click="loadCategories(categorie)">{{categorie.categ_nom}}</button>
+			ng-class="{'btn-default': categoriesPath.indexOf(categorie) == -1,
+				'btn-primary': categoriesPath.indexOf(categorie) > -1}"
+			ng-click="clickOnCategorie(categorie)">{{categorie.categ_nom}}</button>
 	</li>
 </ul>
 
--- tuningfork/public/js/app-admin.js
+++ tuningfork/public/js/app-admin.js
@@ -1,3 +1,100 @@
+tfApp.controller('AdminAddInstrumentCtrl', ['$scope', 'utilities', function ($scope, $http){
+
+	$scope.instru = {
+		categpath: []
+	};
+
+	$scope.addcateg  = false;
+	$scope.addtype  = false;
+	$scope.addmarque = false;
+
+	$scope.addCateg = function(){
+
+		$http({
+			method: 'post',
+			url: '/admin/instruments/addCategorie/',
+			data: 'newcateg=' + $scope.newcateg + '&parent=' + $scope.instru.categpath[0].categ_id,
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+		}).success(function(data){
+			$scope.results = data;
+			if($scope.results.success) {
+				$scope.loadCategs();
+				$scope.addcateg = false;
+				$scope.newcateg = '';
+				$scope.instru.categ_id = $scope.results.categid.toString();
+			}
+		}).then(function(){
+			return false;
+		});
+	}
+
+	$scope.addMarque = function(){
+
+		$http({
+			method: 'post',
+			url: '/admin/instruments/addMarque/',
+			data: 'newmarque=' + $scope.newmarque ,
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+		}).success(function(data){
+			$scope.results = data;
+			if($scope.results.success) {
+				$scope.loadMarques();
+				$scope.addmarque = false;
+				$scope.newmarque = '';
+				$scope.instru.marque_id = $scope.results.marqueid.toString();
+			}
+		}).then(function(){
+			return false;
+		});
+
+	}
+
+	$scope.loadCategs = function(){
+		// categ = $scope.instru.categpath.slice(-1)[0].categ_id;
+		categ = $scope.instru.categpath.length > 0 ? $scope.instru.categpath[0].categ_id:'';
+		// console.log(categ);
+		$http.get('/admin/instruments/getCategories/' + categ + '/ajax').success(function(data){
+			// console.log(data);
+			$scope.categories = data;
+		},true);
+	}
+
+	$scope.loadMarques = function(){
+		$http.get('/admin/instruments/getMarques/').success(function(data){
+			$scope.marques = data;
+		},true);
+
+	}
+
+	$scope.removeLeafCateg = function(){
+		$scope.instru.categpath.shift();
+		$scope.categorie = $scope.instru.categpath[0];
+	}
+
+	$scope.removeCateg = function(categ){
+		var index = $scope.instru.categpath.indexOf(categ);
+		$scope.instru.categpath.splice(0, index+1);
+		$scope.categorie = $scope.instru.categpath[0];
+	}
+
+	$scope.loadCategs();
+	$scope.loadMarques();
+
+	$scope.$watch('categorie', function(){
+		if ( $scope.categorie !== undefined && $scope.instru.categpath.indexOf($scope.categorie) == -1 ) {
+			$scope.instru.categpath.unshift($scope.categorie);
+		}
+		$scope.loadCategs();
+	}, true);
+
+	$scope.$watch('addmarque', function(){
+		if($scope.addmarque) {
+			newinstrument.newmarque.focus();
+		}
+	});
+
+}]);
+
 tfApp.controller('AdminListInstruCtrl', ['$scope', '$http', '$filter', '$q', 'ngTableParams', function ($scope, $http, $filter, $q, ngTableParams){
 
 	$scope.instruments = [];
@@ -116,35 +213,66 @@ tfApp.controller('AdminListCategCtrl', function ($scope, $http, $filter) {
     $scope.categories = [];
     $scope.categoriesPath = [];
 
+    $scope.showNewCategField = false;
+
     $scope.loadCategories = function(parent) {
 
-        console.log($scope.categoriesPath);
-
-        if ( $scope.categoriesPath.length ) {
-            angular.forEach($scope.categoriesPath, function(item, index){
-                console.log(item);
-                if ( item.parent_id == parent.parent_id ) {
-                    $scope.categoriesPath.splice(index, $scope.categoriesPath.length-pos);
-                    $scope.categories.splice(index, $scope.categories.length-pos);
-                }
-            });
-        }
-
-        var pos = $scope.categoriesPath.indexOf(parent);
-        if ( pos > -1 ) {
-            $scope.categoriesPath.splice(pos, $scope.categoriesPath.length-pos);
-            $scope.categories.splice(pos, $scope.categories.length-pos);
-        } else {
-            $scope.categoriesPath.push(parent);
-             $http.get('/admin/instruments/getCategories/'+(parent||null)+'/ajax').success(function(data){
-                $scope.categories.push(data.categories);
-                console.debug($scope.categories);
-            },true);
-       }
+    	var categid = parent ? parent.categ_id : null;
+        $http.get('/admin/instruments/getCategories/'+categid+'/ajax').success(function(data){
+            $scope.categories.push(data.categories);        		
+        },true);
 
     }
 
-        console.log($scope.categoriesPath);
+    $scope.clickOnCategorie = function(parent){
+
+    	$scope.showNewCategField = false;
+
+        var pos = $scope.categoriesPath.indexOf(parent);
+        var niveau;
+        angular.forEach($scope.categories, function(level, index){
+        	if ( level.indexOf(parent) > -1) {
+        		niveau = index;
+        	}
+        });
+
+        if ( niveau !== undefined && niveau <= $scope.categories.length) {
+            $scope.categoriesPath.splice(niveau, $scope.categoriesPath.length-niveau);
+            $scope.categories.splice(niveau+1, $scope.categories.length-niveau);
+        }
+
+        if ( pos > -1 ) {
+            $scope.categoriesPath.splice(pos, $scope.categoriesPath.length-pos);
+            $scope.categories.splice(pos+1, $scope.categories.length-pos);
+        } else {
+	        $scope.categoriesPath.push(parent);
+			$scope.loadCategories(parent);
+	    }
+    }
+
+	$scope.addCateg = function(level){
+
+		console.log($scope.newcateg);
+		return;
+
+		$http({
+			method: 'post',
+			url: '/admin/instruments/addCategorie/',
+			data: 'newcateg=' + $scope.newcateg + '&parent=' + $scope.categoriesPath[level-1].categ_id,
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+		}).success(function(data){
+			$scope.results = data;
+			if($scope.results.success) {
+				$scope.showNewCategField = false;
+				$scope.newcateg = '';
+			}
+		});
+	}
+
+    // $scope.addCateg = function(newcateg, level) {
+    // 	console.log(newcateg + ' in level '+level+' with parent ' + $scope.categoriesPath[level-1].categ_id);
+    // }
+
     $scope.loadCategories();
 
 });
