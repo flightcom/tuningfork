fe0244a19b3144a2711df0d9bc0fc5ff68c72a9e
sebastien <flightcom@wanadoo.fr>
2015-09-08 07:47:09 +0200

    commit home 20150908@0747

--- tuningfork/application/views/admin/master.php
+++ tuningfork/application/views/admin/master.php
@@ -27,16 +27,26 @@
 
 			<nav id="topbar" class="navbar navbar-fixed-top navbar-inverse col-xs-12 nopadding" role="navigation">
 
-				<div class="navbar-header col-sm-2 col-lg-1 nopadding col-xs-hidden">
+				<div class="navbar-header col-sm-2 nopadding col-xs-hidden">
 					<a class="navbar-brand pull-left color-sweet-green" href="/">tuningfork</a>
 				</div>
 
-				<div class="navbar-header col-xs-12 col-sm-8 col-lg-9">
+				<div class="navbar-header col-xs-12 col-sm-8">
 
 					<form action="/admin/recherche/" method="post" class="pdbt-8" role="search">
-						<div class="form-group col-xs-12 nomargin nopadding">
-							<input type="text" id="search" name="search" class="form-control col-xs-12" placeholder="Rechercher...">
-						</div>
+						<div class="input-group">
+							<input type="text" id="search" name="search" class="form-control" placeholder="Rechercher...">
+							<span class="input-group-btn">
+								<button type="button" class="btn btn-primary dropdown-toggle"
+									data-toggle="dropdown"><span class="glyphicon glyphicon-plus"></span>
+								</button>
+							    <ul class="dropdown-menu" role="menu">
+							        <li><a href="/admin/prets/add">Prêt</a></li>
+							        <li><a href="/admin/membres/add">Membre</a></li>
+							        <li><a href="/admin/instruments/add">Instrument</a></li>
+							    </ul>
+							</span>
+  						</div>
 					</form>
 
 				</div>
@@ -47,11 +57,11 @@
 
 			<div id="middlebar" class="navbar col-xs-12 nomargin nopadding" role="navigation" ng-controller="MenuCtrl">
 
-				<div class="navbar-header col-xs-hidden col-sm-2 col-lg-1 nopadding">
+				<div class="navbar-header col-xs-hidden col-sm-2 nopadding">
 					<a class="navbar-brand" href="" ng-click="menu.set(!menu.visible())"><span class="glyphicon glyphicon-menu-hamburger"></span> Accueil</a>
 				</div>
 
-				<div class="col-xs-5 col-sm-6 col-lg-7 nopadding">
+				<div class="col-xs-5 col-sm-6 nopadding">
 					<ul class="nav menu navbar-nav breadcrumb">
 						<?php echo $this->breadcrumb->toHTML(); ?>
 					</ul>
@@ -66,11 +76,11 @@
 
 			<div id="wrap" class="col-xs-12" role="main" ng-controller="MenuCtrl">
 
-				<div class="sidebar col-xs-12 col-sm-2 col-lg-1" ng-show="menu.visible()" id="sidebar-left">
+				<div class="sidebar col-xs-12 col-sm-2 pdt-20" ng-show="menu.visible()" id="sidebar-left">
 					<?php echo $this->dashboard; ?>
 				</div>
 
-				<div id="content" class="admin pull-right" class="col-xs-12" ng-class="{'col-sm-10 col-lg-11': menu.visible(), 'col-sm-12': !menu.visible()}">
+				<div id="content" class="admin pull-right" class="col-xs-12" ng-class="{'col-sm-10': menu.visible(), 'col-sm-12': !menu.visible()}">
 					<?php echo $content; ?>
 				</div>
 
