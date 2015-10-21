43cd82cf78ef0400a2a015614d1c021202bac464
sebastien <sebastien.a.moreau@capgemini.com>
2015-10-21 09:19:35 +0200

    commit cg 20151021@0920

--- tuningfork/application/config/database.php
+++ tuningfork/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = 'tooroot';
 $db['default']['database'] = 'tuningfork';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
