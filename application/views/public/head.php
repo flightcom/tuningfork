a573a31167cfb679497a03c23c4ebdb67ce1350a
Sebastien Moreau <sebastien.moreau@rocketmail.com>
2016-11-04 10:02:55 -0400

    Externalized <head> from master

---
+++ application/views/public/head.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="public/dist/img/favicon.ico" />
+
+    <base href="/">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="<?php echo (CSS_FILE); ?>" rel="stylesheet" type="text/css" />
+
+</head>
--- application/views/public/master.php
+++ application/views/public/master.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-	<head>
-		<meta charset="utf-8">
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <link rel="icon" type="image/png" href="public/dist/img/favicon.ico" />
-
-		<base href="/">
-
-		<title><?php echo $title; ?></title>
-
-		<link href="<?php echo (CSS_FILE); ?>" rel="stylesheet" type="text/css" />
-
-	</head>
+    <?php $this->load->view('public/head'); ?>
 
 	<body ng-app="app">
 
-		<nav class="navbar navbar-fixed-top">
-			<div class="navbar-header">
-				<a class="navbar-brand nopadding" href="/"><img src="<?php echo IMG_PATH; ?>/logo_long_small.png" height="30"></a>
-			</div>
+		<nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand nopadding" href="/"><img src="<?php echo IMG_PATH; ?>/logo_long_small.png" height="30"></a>
+                </div>
 
-			<ul id="main-menu" class="nav nav-pills">
-				<li class="nav-item"><a class="nav-link" href="#asso">L'Association</a></li>
-				<!-- <li class="nav-item"><a class="nav-link" href="#actus">Actualités</a></li> -->
-				<li class="nav-item"><a class="nav-link" href="#instrumentheque">L'Instrumenthèque</a></li>
-				<li class="nav-item"><a class="nav-link" href="#stations">Stations musicales</a></li>
-				<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-				<!-- <li class="nav-item"><a class="nav-link" href="#jeparticipe">Je participe !</a></li> -->
-				<!-- <li class="nav-item"><a class="nav-link" href="/blog">Blog</a></li> -->
-			</ul>
-
-			<?php // echo $this->session->userdata('account'); ?>
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul id="main-menu" class="nav nav-pills">
+                        <li class="nav-item"><a class="nav-link" href="#asso">L'Association</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link" href="#actus">Actualités</a></li> -->
+                        <li class="nav-item"><a class="nav-link" href="#instrumentheque">L'Instrumenthèque</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#stations">Stations musicales</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link" href="#jeparticipe">Je participe !</a></li> -->
+                        <!-- <li class="nav-item"><a class="nav-link" href="/blog">Blog</a></li> -->
+                    </ul>
+                    <?php // echo $this->session->userdata('account'); ?>
+                </div>
+            </div>
 
 		</nav>
 
