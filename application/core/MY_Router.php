4faa56bdea8114d73d8a4ac4e89da1b01a663b30
Sebastien Moreau <sebastien.moreau@rocketmail.com>
2017-02-11 15:07:50 -0500

    Added new router

---
+++ application/core/MY_Router.php
@@ -0,0 +1,33 @@
+<?php
+
+class MY_Router extends CI_Router
+{
+    public function set_class($class)
+    {
+        parent::set_class($this->_rewriteSegment($class));
+    }
+
+    public function set_method($method)
+    {
+        parent::set_method($this->_rewriteSegment($method));
+    }
+
+    public function _validate_request($segments)
+    {
+        foreach ($segments as &$segment) {
+            if (!isset($segment)) continue;
+            $segment = $this->_rewriteSegment($segment);
+        }
+
+        return parent::_validate_request($segments);
+    }
+
+    protected function _rewriteSegment($segment)
+    {
+        return preg_replace_callback('/-\w/', function ($chars) {
+            error_log(print_r($chars, true));
+            return strtoupper(str_replace('-', '', $chars[0]));
+        }, $segment);
+    }
+
+}
